b0db8ee2af4ac3b8c47793bd91b9e05e108649ce
Michaelcho1201 <112720776+Michaelcho1201@users.noreply.github.com>
2026-06-08 18:32:34 -0700

    update game loop

--- game_loop.py
+++ game_loop.py
@@ -1,20 +1,29 @@
 from diffculty import adjust_difficulty
 from diffculty import generate_pokemon_team
 from battle_loop import run_battle
+from player_team import player_team
+
+
+def reset_player_team_hp():
+    for pokemon in player_team:
+        pokemon["current_hp"] = pokemon["max_hp"]
+
 
 def start_game():
     difficulty_level = 0
     keep_playing = True
 
     while keep_playing:
+        reset_player_team_hp()
+
         opponent_team = generate_pokemon_team(difficulty_level)
 
         battle_result = run_battle(opponent_team, difficulty_level)
 
         difficulty_level = adjust_difficulty(
             difficulty_level,
-            battle_result.player_won,
-            battle_result.turn_count
+            battle_result["player_won"],
+            battle_result["moves"]
         )
 
         print("New difficulty level: " + str(difficulty_level))
@@ -23,5 +32,6 @@ def start_game():
 
         if answer != "yes":
             keep_playing = False
+
 
 start_game()
