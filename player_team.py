8f147dca069cbe8e646df6373ea6a02e28c16437
Michaelcho1201 <112720776+Michaelcho1201@users.noreply.github.com>
2026-06-08 18:33:50 -0700

    update player team

--- player_team.py
+++ player_team.py
@@ -1,54 +1,92 @@
 player_team = [
     {
         "name": "Pikachu",
+        "types": ["Electric"],
+        "current_hp": 100,
+        "max_hp": 100,
         "item": "Light Ball",
         "ability": "Static",
         "nature": "Timid",
-        "moves": ["Thunderbolt", "Quick Attack", "Iron Tail", "Electro Ball"]
+        "moves": [
+            {"name": "Thunderbolt", "type": "Electric", "power": 90},
+            {"name": "Quick Attack", "type": "Normal", "power": 40},
+            {"name": "Iron Tail", "type": "Steel", "power": 100},
+            {"name": "Electro Ball", "type": "Electric", "power": 80}
+        ]
     },
     {
         "name": "Charizard",
+        "types": ["Fire", "Flying"],
+        "current_hp": 150,
+        "max_hp": 150,
         "item": "Charcoal",
         "ability": "Blaze",
         "nature": "Modest",
-        "moves": ["Flamethrower", "Air Slash", "Dragon Claw", "Slash"]
+        "moves": [
+            {"name": "Flamethrower", "type": "Fire", "power": 90},
+            {"name": "Air Slash", "type": "Flying", "power": 75},
+            {"name": "Dragon Claw", "type": "Dragon", "power": 80},
+            {"name": "Slash", "type": "Normal", "power": 70}
+        ]
     },
     {
         "name": "Blastoise",
+        "types": ["Water"],
+        "current_hp": 160,
+        "max_hp": 160,
         "item": "Mystic Water",
         "ability": "Torrent",
         "nature": "Bold",
-        "moves": ["Water Pulse", "Bite", "Protect", "Rapid Spin"]
+        "moves": [
+            {"name": "Hydro Pump", "type": "Water", "power": 110},
+            {"name": "Bite", "type": "Dark", "power": 60},
+            {"name": "Rapid Spin", "type": "Normal", "power": 50},
+            {"name": "Aqua Tail", "type": "Water", "power": 90}
+        ]
     },
     {
         "name": "Venusaur",
+        "types": ["Grass", "Poison"],
+        "current_hp": 155,
+        "max_hp": 155,
         "item": "Miracle Seed",
         "ability": "Overgrow",
         "nature": "Calm",
-        "moves": ["Razor Leaf", "Sleep Powder", "Leech Seed", "Vine Whip"]
+        "moves": [
+            {"name": "Razor Leaf", "type": "Grass", "power": 55},
+            {"name": "Sludge Bomb", "type": "Poison", "power": 90},
+            {"name": "Tackle", "type": "Normal", "power": 40},
+            {"name": "Seed Bomb", "type": "Grass", "power": 80}
+        ]
     },
     {
         "name": "Lucario",
+        "types": ["Fighting", "Steel"],
+        "current_hp": 145,
+        "max_hp": 145,
         "item": "Black Belt",
         "ability": "Inner Focus",
         "nature": "Adamant",
-        "moves": ["Aura Sphere", "Metal Claw", "Quick Attack", "Detect"]
+        "moves": [
+            {"name": "Aura Sphere", "type": "Fighting", "power": 80},
+            {"name": "Metal Claw", "type": "Steel", "power": 50},
+            {"name": "Quick Attack", "type": "Normal", "power": 40},
+            {"name": "Bone Rush", "type": "Ground", "power": 75}
+        ]
     },
     {
         "name": "Snorlax",
+        "types": ["Normal"],
+        "current_hp": 220,
+        "max_hp": 220,
         "item": "Leftovers",
         "ability": "Immunity",
         "nature": "Careful",
-        "moves": ["Body Slam", "Rest", "Crunch", "Heavy Slam"]
+        "moves": [
+            {"name": "Body Slam", "type": "Normal", "power": 85},
+            {"name": "Crunch", "type": "Dark", "power": 80},
+            {"name": "Heavy Slam", "type": "Steel", "power": 100},
+            {"name": "Tackle", "type": "Normal", "power": 40}
+        ]
     }
 ]
-
-def show_player_team():
-    print("Player Team:")
-    for i in range(len(player_team)):
-        pokemon = player_team[i]
-        print(str(i + 1) + ". " + pokemon["name"])
-        print("   Item: " + pokemon["item"])
-        print("   Ability: " + pokemon["ability"])
-        print("   Nature: " + pokemon["nature"])
-        print("   Moves: " + str(pokemon["moves"]))
